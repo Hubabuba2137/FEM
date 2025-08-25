3fc507405b9641e293ba9bddaf78d89287ee9070
Hubert Stach <hubertstach@student.agh.edu.pl>
2025-08-26 00:08:10 +0200

    xd

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@
 
 int main()
 {
-    std::string filename = "C:/Users/marci/Desktop/inzynierka/kod/MES/Test1_bcs.txt";
-    //std::string filename = "C:/Users/marci/Desktop/inzynierka/kod/MES/Test3_31_31_kwadrat.txt";
+    std::string filename = "../Test1_bcs.txt";
+    //std::string filename = "../Test3_31_31_kwadrat.txt";
     
     Fem::Solution mes_solution(filename);
     mes_solution.solve(true, true);
